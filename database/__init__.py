5fafa5fbe356a236307a16a147d27dfe30509038
tianxiu2b2t <atianxiua@163.com>
2025-01-30 05:04:54 +0800

    fix: 修复数据库本地了

--- database/__init__.py
+++ database/__init__.py
@@ -3,5 +3,5 @@ import motor.motor_asyncio as amotor
 import urllib.parse as urlparse
 
 db = amotor.AsyncIOMotorClient(
-    f"mongodb://{urlparse.quote(config.env.get('MONGO_USER') or '')}:{urlparse.quote(config.env.get('MONGO_PASSWORD') or '')}@localhost:27017"
+    f"mongodb://{urlparse.quote(config.env.get('MONGO_USER') or '')}:{urlparse.quote(config.env.get('MONGO_PASSWORD') or '')}@{config.env.get('MONGO_HOST') or 'localhost'}:{config.env.get('MONGO_PORT') or '27017'}"
 ).get_database(config.env.get("DB") or "Proxy")
--- web/core/common.py
+++ web/core/common.py
@@ -129,7 +129,7 @@ class Statistics:
             objs = self._db_access_logger.copy()
             for obj in objs:
                 self._db_access_logger.remove(obj)
-            await collection.insert_many([{
+            print(await collection.insert_many([{
                 "_id": obj.id,
                 "type": obj.type,
                 "host": obj.host,
@@ -141,7 +141,7 @@ class Statistics:
                 "user_agent": obj.user_agent,
                 "http_version": obj.http_version,
                 "utc_time": obj.utc_time
-            } for obj in objs])
+            } for obj in objs]))
     
     def start(self):
         self._logger_task = asyncio.get_running_loop().create_task(self._task_print_access_log())
